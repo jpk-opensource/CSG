5e209973dcc7751d16ab51ea079fac86eeb00918
Jithin Renji <jithin0904@gmail.com>
2020-10-29 16:44:21 +0400

    Add copyright info in version info output

--- csg.py
+++ csg.py
@@ -65,9 +65,12 @@ def usage():
 def version():
     print(f"""\
 CSG {VERSION}
+Copyright (C) 2020 Jithin Renji, Kannan MD, and Pranav Pujar
 License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.
 This is free software: you are free to change and redistribute it.
-There is NO WARRANTY, to the extent permitted by law.\
+There is NO WARRANTY, to the extent permitted by law.
+
+Written by Jithin Renji, Kannan MD, and Pranav Pujar.\
 """)
 
 
